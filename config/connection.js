cb99bc66e0f061e9ca47881a8b6a620a8dd2fde2
Alvin Go <alvingogo25@gmail.com>
2018-03-12 19:39:35 -0700

    added local connection config

--- config/connection.js
+++ config/connection.js
@@ -6,15 +6,19 @@ var mysql = require("mysql");
 
 var password = keys.password.key;
 
-var connection = mysql.createConnection(process.env.JAWSDB_URL
-//   {
-//   port: 3306,
-//   host: "localhost",
-//   user: "root",
-//   password: password,
-//   database: "burgers_db"
-// }
-);
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: password,
+    database: "burgers_db"
+  });
+}
+
 
 // Make connection.
 connection.connect(function(err) {
